aed2e4cf6130c596210b9b61940fcbeb33ab5c15
Chris Lundquist <rampantdurandal@gmail.com>
2010-12-11 01:09:38 -0800

    added test target to make and test files for JobQueue and Job

---
+++ java/src/JobQueueTest.java
@@ -0,0 +1,7 @@
+class JobQueueTest {
+    private static final String TEST_FILE = "test.queue";
+
+    static public void main(String[] args){
+        JobQueue queue = new JobQueue();
+    }
+}
---
+++ java/src/JobTest.java
@@ -0,0 +1,5 @@
+class JobTest {
+    static public void main(String[] args){
+        Job job = new Job();
+    }
+}
