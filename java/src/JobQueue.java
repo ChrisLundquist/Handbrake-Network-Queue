61370d6fbd81e02e5905c8c6923d960c7d253f47
Chris Lundquist <rampantdurandal@gmail.com>
2010-12-12 02:13:20 -0800

    remove uneeded import

--- java/src/JobQueue.java
+++ java/src/JobQueue.java
@@ -5,7 +5,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
-//import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
