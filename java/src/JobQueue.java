1fb77c3846bb14eadf07985aa24a36abe164f762
Chris Lundquist <rampantdurandal@gmail.com>
2010-12-11 16:33:09 -0800

    Server and Jobqueue now are connected

--- java/src/JobQueue.java
+++ java/src/JobQueue.java
@@ -1,9 +1,16 @@
 // Manages Jobs into a queue
-public class JobQueue {
-    java.util.Vector<Job> queue;
+import java.beans.XMLEncoder;
+import java.beans.XMLDecoder;
+import java.io.*;
 
-    JobQueue() {
-        //TODO
+public class JobQueue {
+    private java.util.Vector<Job> queue;
+    private XMLDecoder in;
+    private XMLEncoder out;
+
+    JobQueue(String filePath) throws IOException{
+        in = new XMLDecoder(new BufferedInputStream(new FileInputStream(filePath)));
+        System.out.println(in);
     }
 
     public Job getNextJob(){
@@ -11,11 +18,11 @@ public class JobQueue {
             Job job = queue.get(i);
             // Skip the jobs that are complete or canceled
             if(job.isComplete() || job.isCanceled())
-                continue;
-            // If we have a new Job return the first new job we find
-            if(job.isNew())
-                return job;
-            // TODO Return a checked out job
+            continue;
+        // If we have a new Job return the first new job we find
+        if(job.isNew())
+            return job;
+        // TODO Return a checked out job
         }
         // We couldn't find a job
         return null;
--- java/src/Server.java
+++ java/src/Server.java
@@ -5,10 +5,25 @@ public class Server {
     boolean listening;
     int port;
     ServerSocket server;
+    private String queueFile;
+    public static JobQueue queue;
+    public static int DEFAULT_PORT = 4444;
 
-    Server(int port){
+    Server(String queueFilePath){
+        this(queueFilePath,DEFAULT_PORT);
+    }
+
+    Server(String queueFilePath, int port){
         listening = true;
+        queueFile = queueFilePath;
         this.port = port;
+        try{
+        queue = new JobQueue(queueFile);
+        } catch(IOException e){
+            System.err.println("Could not open the queueFile specified: " + queueFile);
+            System.err.println(e);
+            System.exit(-2);
+        }
     }
 
     void stop(){
@@ -19,9 +34,9 @@ public class Server {
         ServerSocket server = null;
         // Try to establish a server
         try {
-            server = new ServerSocket(4444);
+            server = new ServerSocket(port);
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 4444");
+            System.err.println("Could not listen on port: " + port);
             System.exit(-1);
         }
 
@@ -30,13 +45,35 @@ public class Server {
             while(listening)
                 (new ServerThread(server.accept())).run();
         } catch (IOException e) {
-            System.err.println("Accept failed: 4444");
+            System.err.println("Accept failed: " + port);
             System.exit(-1);
         }
     }
 
+    private static void usage(){
+        System.out.println(" remote_host_name_or_ip listening_port");
+    }
+
     public static void main(String[] args){
-        Server server = new Server(4444);
+        Server server = null;
+        String filePath = null;
+        int port;
+
+        if(args.length == 0){
+            System.err.println("No queue file specified");
+            usage();
+            System.exit(-1);
+        }
+        
+        if(args.length == 1){
+            filePath = args[0];
+            server = new Server(filePath);
+        }
+
+        if(args.length == 2){
+            port = Integer.parseInt(args[1]);
+            server = new Server(filePath,port);
+        }
         server.start();
     }
 }
