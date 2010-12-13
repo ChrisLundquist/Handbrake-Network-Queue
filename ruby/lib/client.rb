f528a4e9d4034a95339e3c9732e89392d8b714de
Chris Lundquist <rampantdurandal@gmail.com>
2010-12-12 17:27:43 -0800

    a client that is a bit more robost

--- ruby/lib/client.rb
+++ ruby/lib/client.rb
@@ -20,15 +20,12 @@ class Client
 
     # Main function to be called
     def run
-        connect()
-        get_job()
-        disconnect()
-        connect()
-        do_job()
-        disconnect()
-        connect()
-        complete_job()
-        disconnect()
+        while (get_job() ) do
+            checkout_job()
+              checkout_job()
+            do_job()
+            complete_job()
+        end
     end
 
     private
@@ -43,26 +40,43 @@ class Client
     end
 
     def get_job()
-        puts "getting job with command: #{Command::GET_JOB}"
+        connect()
         @server.write(Command::GET_JOB)
         f = File.new("test.job","w")
         response = @server.recvmsg.first
         response.chomp!
-        @job = YAML.load(response)
-        f.write(@job.to_yaml)
-        f.close
+        if response == Command::NO_JOB
+            disconnect()
+            return false
+        else
+            @job = YAML.load(response)
+            f.write(@job.to_yaml)
+            f.close
+            disconnect()
+        end
+        return true
+    end
+
+    def checkout_job()
+      puts "Checking out job id #{@job.id}"
+      connect()
+      @server.write(Command::CHECKOUT_JOB)
+      @server.write(@job.id)
+      disconnect()
     end
 
     def do_job()
-        puts "doing job"
+        puts "Doing job id #{@job.id}"
         #TODO 
     end
 
     # Write the command
     # Then the Job ID
     def complete_job()
-        puts "completing job"
+        puts "Completing job id #{@job.id}"
+        connect()
         @server.write(Command::COMPLETE_JOB)
         @server.write(@job.id)
+        disconnect()
     end
 end
